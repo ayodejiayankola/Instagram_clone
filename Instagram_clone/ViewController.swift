62ff0e35996565f5fcbb411ed9acc4a9041ccac1
ayodejiayankola <ayodejiayankola@gmail.com>
2020-12-20 11:37:26 +0100

    Login Screen completed

---
+++ Instagram_clone/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Instagram_clone
+//
+//  Created by Ayodeji Ayankola on 12/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func loginWithGooglePressed(_ sender: UIButton) {
+        
+        print("Redirect to another Screen")
+        
+    }
+    
+    
+    
+}
+
